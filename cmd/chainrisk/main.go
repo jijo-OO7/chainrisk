74341c13ca50c6e63b0d8db1c8ec40a21bdd31f3
Suman Mandal <sumanmandal.intro@gmail.com>
2026-03-10 07:52:30 +0000

    initial project structure for chainrisk CLI

---
+++ cmd/chainrisk/main.go
@@ -0,0 +1,8 @@
+package main
+
+import ( "fmt"
+)
+
+func main(){
+	fmt.Println("chainrisk - probabilistic supply chain risk analysis")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jijo-OO7/chainrisk.git
+
+go 1.22.2
